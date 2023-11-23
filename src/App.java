1b713e9623972bb9440d217ec109790fb203e334
ManishReddy10 <122648051+ManishReddy10@users.noreply.github.com>
2023-11-22 22:26:12 -0700

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int num[][] = new int[3][];
+        
+        char[][] alphabet = {
+            {'a', 'b', 'c'}, 
+            {'d', 'e'},
+            {'f'}
+        };
+
+        System.out.println(alphabet.length);
+        for (int i = 0; i < alphabet.length; i++) {
+            for (int j = 0; j < alphabet[i].length; j++) {
+                System.out.println(alphabet[i][j]);
+            }
+        }
+
+
+    }
+}
